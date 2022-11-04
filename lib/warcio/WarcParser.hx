31719ba6fd9dbc65eb93cd376f5e2e70ff0dfc25
Dizbdeedee <pono@mentice.cotse.net>
2022-11-04 17:08:27 +0000

    Move created externs

---
+++ lib/warcio/WarcParser.hx
@@ -0,0 +1,7 @@
+@:jsRequire("warcio", "WARCParser")
+extern class WARCParser {
+	function new(source:Dynamic,?opt:Dynamic);
+
+	function parse():js.lib.Promise<Null<WARCResult>>;
+
+}
---
+++ lib/warcio/WarcResult.hx
@@ -0,0 +1,9 @@
+extern class WARCResult {
+	function readFully():js.lib.Promise<Null<Dynamic>>;
+
+	var warcTargetURI:String;
+
+	var payload:js.lib.Uint8Array;
+
+	var warcType:String;
+}
